032056e32cab88c7e66dba31dcdec3750475d55c
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-05-18 12:21:37 +0100

    Add mergesort.ts to sort arrays

---
+++ mergesort.ts
@@ -0,0 +1,43 @@
+function merge(sortedLeftArr: number[], sortedRightArr: number[]) {
+  let i = 0;
+  let j = 0;
+  const sortedArr: number[] = [];
+  while (sortedLeftArr[i] || sortedRightArr[j]) {
+    const leftEl = sortedLeftArr[i];
+    const rightEl = sortedRightArr[j];
+    if (!leftEl) {
+      sortedArr.push(rightEl);
+      j++;
+    }
+    if (!rightEl) {
+      sortedArr.push(leftEl);
+      i++;
+    }
+    if (leftEl && rightEl) {
+      if (leftEl < rightEl) {
+        sortedArr.push(leftEl);
+        i++;
+      } else {
+        sortedArr.push(rightEl);
+        j++;
+      }
+    }
+  }
+
+  return sortedArr;
+}
+
+export function mergeSort(arr: number[]) {
+  const n = arr.length;
+  if (n <= 1) return arr;
+
+  const m = Math.floor(n / 2);
+
+  const leftArr = arr.slice(0, m);
+  const rightArr = arr.slice(m, n);
+
+  const sortedLeftArr = mergeSort(leftArr);
+  const sortedRightArr = mergeSort(rightArr);
+
+  return merge(sortedLeftArr, sortedRightArr);
+}
