f6e3c8e6b6b5b7dcdd32bdf31a623049ae99fdd0
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-05-22 07:16:17 +0100

    Add insert method to tree class

--- index.ts
+++ index.ts
@@ -4,3 +4,5 @@ const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = new Tree();
 tree.buildTree(arr);
 tree.prettyPrint();
+tree.insert(320);
+tree.prettyPrint();
--- tree.ts
+++ tree.ts
@@ -2,67 +2,91 @@ import { Node } from "./node";
 import { mergeSort } from "./mergesort";
 
 export class Tree {
-  private _root: Node;
+	private _root: Node;
 
-  private _removeDuplicates(sortedArr: number[]) {
-    let previous: number | null = null;
-    for (let i = 0; i < sortedArr.length; i++) {
-      if (sortedArr[i] === previous) {
-        sortedArr.splice(i, 1);
-        i--;
-      } else {
-        previous = sortedArr[i];
-      }
-    }
-    return sortedArr;
-  }
+	private _removeDuplicates(sortedArr: number[]) {
+		let previous: number | null = null;
+		for (let i = 0; i < sortedArr.length; i++) {
+			if (sortedArr[i] === previous) {
+				sortedArr.splice(i, 1);
+				i--;
+			} else {
+				previous = sortedArr[i];
+			}
+		}
+		return sortedArr;
+	}
 
-  private _buildTree(sortedArr: number[]): Node {
-    const len = sortedArr.length;
-    const newNode = new Node();
+	private _buildTree(sortedArr: number[]): Node {
+		const len = sortedArr.length;
+		let newNode: Node | null = new Node();
 
-    if (len <= 1) {
-      if (sortedArr[0]) newNode.value = sortedArr[0];
-      newNode.leftNode = null;
-      newNode.rightNode = null;
-    } else {
-      const mid = Math.floor(len / 2);
-      newNode.value = sortedArr[mid];
-      const leftArr = sortedArr.slice(0, mid);
-      const rightArr = sortedArr.slice(mid + 1, len);
-      newNode.leftNode = this._buildTree(leftArr);
-      newNode.rightNode = this._buildTree(rightArr);
-    }
+		if (len <= 1) {
+			if (sortedArr[0] || sortedArr[0] === 0) newNode.value = sortedArr[0];
+			else newNode = null;
+			if (newNode) {
+				newNode.leftNode = null;
+				newNode.rightNode = null;
+			}
+		} else {
+			const mid = Math.floor(len / 2);
+			newNode.value = sortedArr[mid];
+			const leftArr = sortedArr.slice(0, mid);
+			const rightArr = sortedArr.slice(mid + 1, len);
+			newNode.leftNode = this._buildTree(leftArr);
+			newNode.rightNode = this._buildTree(rightArr);
+		}
 
-    return newNode;
-  }
+		return newNode as Node;
+	}
 
-  prettyPrint(node = this._root, prefix = "", isLeft = true) {
-    if (node === null) {
-      return;
-    }
-    if (node.rightNode !== null) {
-      this.prettyPrint(
-        node.rightNode,
-        `${prefix}${isLeft ? "│   " : "    "}`,
-        false
-      );
-    }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
-    if (node.leftNode !== null) {
-      this.prettyPrint(
-        node.leftNode,
-        `${prefix}${isLeft ? "    " : "│   "}`,
-        true
-      );
-    }
-  }
+	prettyPrint(node = this._root, prefix = "", isLeft = true) {
+		if (node === null) {
+			return;
+		}
+		if (node.rightNode !== null) {
+			this.prettyPrint(
+				node.rightNode,
+				`${prefix}${isLeft ? "│   " : "    "}`,
+				false
+			);
+		}
+		console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+		if (node.leftNode !== null) {
+			this.prettyPrint(
+				node.leftNode,
+				`${prefix}${isLeft ? "    " : "│   "}`,
+				true
+			);
+		}
+	}
 
-  buildTree(arr: number[]) {
-    const sortedArr = mergeSort(arr);
-    this._removeDuplicates(sortedArr);
+	buildTree(arr: number[]) {
+		const sortedArr = mergeSort(arr);
+		this._removeDuplicates(sortedArr);
 
-    this._root = this._buildTree(sortedArr);
-    return this._root;
-  }
+		this._root = this._buildTree(sortedArr);
+		return this._root;
+	}
+
+	insert(value: number) {
+		const node = new Node(value);
+		let currentNode = this._root;
+		let done = false;
+		while (!done) {
+			if (currentNode.value > node.value) {
+				if (!currentNode.leftNode) {
+					currentNode.leftNode = node;
+					done = true;
+				} else currentNode = currentNode.leftNode as Node;
+			} else if (currentNode.value < node.value) {
+				if (!currentNode.rightNode) {
+					currentNode.rightNode = node;
+					done = true;
+				} else currentNode = currentNode.rightNode;
+			} else if (currentNode.value === node.value) done = true;
+		}
+	}
+
+	delete;
 }
