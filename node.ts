abfe47e553c4a742fee859141e8c9a288d82c706
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-05-23 10:47:55 +0100

    Add parent pointer to the node

--- node.ts
+++ node.ts
@@ -2,10 +2,12 @@ export class Node {
   private _value: number;
   private _leftNode: Node | null;
   private _rightNode: Node | null;
-  constructor(value?: any, leftNode?: Node, rightNode?: Node) {
+  private _parent: Node | null;
+  constructor(value?: any, leftNode?: Node, rightNode?: Node, parent?: Node) {
     this._value = value || null;
     this._leftNode = leftNode || null;
     this._rightNode = rightNode || null;
+    this._parent = parent || null;
   }
 
   get value() {
@@ -30,5 +32,13 @@ export class Node {
 
   set rightNode(node: Node | null) {
     this._rightNode = node;
+  }
+
+  get parent() {
+    return this._parent;
+  }
+
+  set parent(node: Node | null) {
+    this._parent = node;
   }
 }
